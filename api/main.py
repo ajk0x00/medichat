0b450a21b89b9d5188be8076cf418116fdcb5be0
Abhijith <ajk0x00@gmail.com>
2025-06-21 12:26:59 +0530

    feat: Add chromadb for storing and retrieving uploaded documents.

--- api/main.py
+++ api/main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI, UploadFile
 
 from utils.pdf_utils import extract_text_from_pdf
+from utils.vector_db import add_text_to_vector_db
 
 app = FastAPI()
 
 
 @app.post("/upload")
 async def read_root(file: UploadFile):
-    with open(f"files/{file.filename}.txt", "wb") as f:
-        if file.content_type == 'application/pdf':
-            file_content = extract_text_from_pdf(f"files/{file.filename}")
-        else:
-            file_content = await file.read()
-        f.write(file_content)
+    if file.content_type == 'application/pdf':
+        file_content = extract_text_from_pdf(f"files/{file.filename}")
+    else:
+        file_content = await file.read()
+    add_text_to_vector_db(file_content)
     return {
         "message": "File uploaded successfully",
         "filename": file.filename
---
+++ api/utils/vector_db.py
@@ -0,0 +1,53 @@
+from chromadb import Client
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter, TokenTextSplitter
+
+chromadb_client = Client()
+embedding_function = SentenceTransformerEmbeddingFunction(
+    model_name="multi-qa-MiniLM-L6-cos-v1"
+)
+medinotes_collection = chromadb_client.get_or_create_collection(
+    name="medinotes",
+    embedding_function=embedding_function,
+)
+charater_splitter = RecursiveCharacterTextSplitter(
+    separators=["\n\n", "\n", ".", " ", ""],
+    chunk_size=512,
+    chunk_overlap=0
+)
+token_splitter = TokenTextSplitter(
+    chunk_size=300,
+    chunk_overlap=50
+)
+
+def add_text_to_vector_db(text: str):
+    """Adds text to the vector database."""
+    if not text:
+        return
+    if isinstance(text, bytes):
+        text = text.decode('utf-8')
+    text = text.strip()
+    # Split the text into manageable chunks
+    chunks = []
+    for chunk in charater_splitter.split_text(text):
+        chunks.extend(token_splitter.split_text(chunk))
+
+    medinotes_collection.add(
+        documents=chunks,
+        ids=[str(hash(text))]
+    )
+
+def query_vector_db(query: str, n_results: int = 5) -> list[str]:
+    """Queries the vector database and returns the top n results."""
+    results = medinotes_collection.query(
+        query_texts=[query],
+        n_results=n_results
+    )
+    return results['documents'][0]
+
+if __name__ == "__main__":
+    sample_text = "This is a sample text to be added to the vector database."
+    add_text_to_vector_db(sample_text)
+    query_results = query_vector_db("sample text")
+    print(query_results)
